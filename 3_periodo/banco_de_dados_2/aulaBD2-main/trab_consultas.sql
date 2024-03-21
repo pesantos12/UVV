abd2efb6b2ec1b781b89a022992adecdae85efae
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2024-03-21 15:40:12 -0300

    Update trab_consultas.sql

--- 3_periodo/banco_de_dados_2/aulaBD2-main/trab_consultas.sql
+++ 3_periodo/banco_de_dados_2/aulaBD2-main/trab_consultas.sql
@@ -17,3 +17,11 @@ WHERE i.cust_id = c.cust_id AND c.cust_id = a.cust_id AND a.open_branch_id = b.b
 AND c.city != b.city;
 
 -- 3. Junção Externa, Agrupamento, Agregação e Ordenação
+SELECT e.fname Nome, COUNT(t.txn_date) AS Transacoes, e.start_date 'Data de entrada'
+FROM employee e
+LEFT JOIN account a ON e.emp_id = a.open_emp_id
+LEFT JOIN transaction t ON a.account_id = t.account_id
+GROUP BY emp_id
+ORDER BY nome, e.start_date;
+
+-- 4. Junções Internas, Agrupamento, Agregação, União e Concatenação
