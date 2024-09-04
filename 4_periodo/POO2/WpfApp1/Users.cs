43e9e7c259114992854395046e5e103b1fe05eae
Pedro Reis <126162932+pesantos12@users.noreply.github.com>
2024-09-04 08:34:21 -0300

    Add files via upload

---
+++ 4_periodo/POO2/WpfApp1/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Users
+    {
+        public string Nome { get; set; } = "";
+        public string Sobrenome { get; set; } = "";
+        public int Idade { get; set; } = 0;
+
+
+    }
+}
