e4fd4e5afd78b08a51743b9727cd95c13ce3d3ee
Pedro Reis <126162932+pesantos12@users.noreply.github.com>
2024-09-06 10:49:08 -0300

    Add files via upload

---
+++ 4_periodo/POO2/aula240906.cs
@@ -0,0 +1,37 @@
+/*
+Arquitetura em Camadas
+
+padrão de projeto = 
+- soluções típicas para problemas comuns no projeto de software
+- proj. pré fabricados que você pode utilizar/personalizar para resolver um problema
+
+descrevendo um padrão=
+- a intenção
+- a motivação
+- a estrutura das classes
+- o exemplo de código
+
+classificação de padrões de proj.
+- Criação = mecanismos de criação de objetos que aumentam a flex. e a 
+reut. do código existente
+- Estruturais = explicam como montar objetos
+- Comportamento = cuidam da comunicação entre objetos
+
+Estruturação e
+
+No início nós temos as aplicações Monolíticas
+
+Depois veio as aplicações em Duas Camadas (bd separado do resto)
+
+Depois veio as aplicações em Três Camadas (front, back e bd)
+
+As camadas promovem:
+- Encapsulamento:
+	Uma camada não vai saber o que tem dentro da outra
+- Coesão
+	componentes parecidos ficam no mesmo grupo
+
+O modelo MVC (Model, View, Control)
+
+
+*/
