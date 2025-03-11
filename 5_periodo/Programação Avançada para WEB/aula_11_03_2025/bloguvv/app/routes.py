95cda5f94ae19c3ad31405f63d3bee4c58f7c7f0
Pedro Reis <126162932+pesantos12@users.noreply.github.com>
2025-03-11 08:37:16 -0300

    Add files via upload

---
+++ 5_periodo/Programação Avançada para WEB/aula_11_03_2025/bloguvv/app/routes.py
@@ -0,0 +1,31 @@
+# app/routes.py
+
+
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Wanderson'}
+    posts = [
+        {
+            'author': {'username': 'Joao'},
+            'body': 'Belo dia em Vila Velha!'
+        },
+        {
+            'author': {'username': 'Maria'},
+            'body': 'Bora para o cinema hoje?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
